ded9b0d70c3774b3c1d42a27831f3790205b1ccd
Gustavo <gstv.330@gmail.com>
2024-04-23 17:13:48 -0300

    inicio serializers/snippet

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'quickstart',
     'rest_framework',
+    'quickstart',
+    'snippets',
 ]
 
 REST_FRAMEWORK = {
